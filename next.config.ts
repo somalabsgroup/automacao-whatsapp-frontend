ee7c7cce560fd0d4316d5f5739c591c40a6f02a9
Alexandre Ramos <alehsilva@live.com>
2026-05-22 13:20:33 -0300

    muda o fluxo para auth com supabase

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
-  env: {
-    NEXT_PUBLIC_BASE_DOMAIN: process.env.NEXT_PUBLIC_BASE_DOMAIN,
-  },
 };
 
 export default nextConfig;
